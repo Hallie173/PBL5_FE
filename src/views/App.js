1746a10e2023e8885feaeafd2fa882aec74f185e
g15 5511 <phuonghaleduy@gmail.com>
2025-04-01 08:59:04 +0700

    .

--- src/views/App.js
+++ src/views/App.js
@@ -14,6 +14,7 @@ import AdminLayout from "../components/AdminLayout/AdminLayout";
 import Restaurant from "../Pages/Food/Restaurant";
 import Users from "../Pages/Admin/Users/Users";
 import Cities from "../Pages/Admin/Cities/Citites";
+import MyTrips from "../components/Mytrips/Mytrips";
 
 function App() {
   return (
