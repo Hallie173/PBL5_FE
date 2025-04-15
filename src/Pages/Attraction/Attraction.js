97ce2a10660eb1421854297816772a8a82fd78f8
Ahrumiki <tranngocminhhoangpct@gmail.com>
2025-04-15 07:28:57 +0700

    them ham search

--- src/Pages/Attraction/Attraction.js
+++ src/Pages/Attraction/Attraction.js
@@ -57,7 +57,9 @@ const Attraction = () => {
                 const nearByRespone = await axios.get(`${BASE_URL}/attractions/topnearby/${attractionId}`);
                 const nearByData = nearByRespone.data.nearbyTopRestaurant;
                 setnearBy(nearByData);
+                
 
+                console.log(nearByData);
                 // Comment lại phần lấy reviews
                 /*
                 // Lấy danh sách reviews
@@ -166,9 +168,9 @@ const Attraction = () => {
                     <h2>Overview</h2>
                     <p className="brief-describe">Welcome to ...</p>
                     <h2>Location</h2>
-                    <div>
+                    {/* <div>
                         <MapComponent address={attraction?.address} />
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
